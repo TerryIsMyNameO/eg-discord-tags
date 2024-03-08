e67b97e845c1a92024dd84ffab16e0503ae1450b
Terry <tnichols0415@gmail.com>
2024-03-08 20:00:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+  // JavaScript for search functionality
+  const searchInput = document.getElementById('search');
+  const gallery = document.getElementById('gallery').children;
+
+  searchInput.addEventListener('input', function() {
+    const searchTerm = this.value.toLowerCase();
+    Array.from(gallery).forEach(item => {
+      const keywords = item.getAttribute('data-keywords').toLowerCase();
+      const name = item.getAttribute('data-name').toLowerCase();
+      if (searchTerm === '' || keywords.includes(searchTerm) || name.includes(searchTerm)) {
+        item.style.display = 'inline-block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  });
+
+  // Add event listener to each image for copying data-command to clipboard
+  document.querySelectorAll('#gallery img').forEach(img => {
+    img.addEventListener('click', function() {
+      const command = this.getAttribute('data-command');
+      copyToClipboard(command);
+      alert('Command copied to clipboard: ' + command);
+    });
+  });
+
+  // Function to copy text to clipboard
+  function copyToClipboard(text) {
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+  }
+
+  // Add event listeners to category links for filtering images
+  document.querySelectorAll('.category-link').forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      // Remove the active class from all category links
+      document.querySelectorAll('.category-link').forEach(link => {
+        link.classList.remove('active-category');
+      });
+      // Add active class to the clicked category link
+      this.classList.add('active-category');
+      const category = this.getAttribute('data-category').toLowerCase();
+      Array.from(gallery).forEach(item => {
+        const itemCategory = item.getAttribute('data-category').toLowerCase();
+        if (category === 'all' || itemCategory === category) {
+          item.style.display = 'inline-block';
+        } else {
+          item.style.display = 'none';
+        }
+      });
+    });
+  });
